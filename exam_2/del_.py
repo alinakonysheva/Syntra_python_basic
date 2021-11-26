10514ddd1c588fc2126e9e3b599e6da432678631
Alina Konysheva <konysheva.alina@gmail.com>
2021-11-26 12:08:08 +0100

    exam, task 2

---
+++ exam_2/del_.py
@@ -0,0 +1,12 @@
+alphabet = 'abcdefghijklmnopqrstuasdlgjhsadgasdgasvwxyz'.upper()
+list_alphabet_big_letters = []
+for letter in alphabet:
+    list_alphabet_big_letters.append(letter)
+set_alphabet_big_letters = set(list_alphabet_big_letters)
+
+print(set_alphabet_big_letters)
+
+
+
+
+print(is_containing_all_letters('abcdefghijklmnopqrstuasdlgjhsadgasdgasvwxyz'))
--- exam_2/task2.py
+++ exam_2/task2.py
@@ -8,3 +8,34 @@
 # “De tweede lijn bevat welk elk letter van alfabet (abcdefghijklmnopqrstuvwxyz)”
 # 3 – Nee
 # “lijn is te kort”
+
+C_NUMBER_LINES = 2
+C_SET_ALPHABET_BIG_LETTERS = {'D', 'T', 'W', 'E', 'R', 'A', 'P', 'V', 'K', 'H', 'L',
+                              'J', 'O', 'X', 'I', 'F', 'Q', 'S', 'B', 'C', 'Y', 'M', 'Z', 'U', 'N', 'G'}
+
+
+def is_containing_all_letters(text: str):
+    origin_text = text
+    if len(text) < 26:
+        return 'Nee', 'lijn is te kort'
+    text = text.upper()
+    list_letter_out_text = []
+    for letter in text:
+        if letter.isalpha():
+            list_letter_out_text.append(letter)
+    try:
+        set_with_all_letters = set(list_letter_out_text)
+        if set_with_all_letters == C_SET_ALPHABET_BIG_LETTERS:
+            return 'Ja. De', 'lijn bevat welk elk letter van alfabet', origin_text
+
+    except Exception as e:
+        return 'Nee, weet niet waarom'
+
+
+with open('file_for_strings.txt', 'a+', encoding='UTF-8') as file:
+    for line in range(C_NUMBER_LINES):
+        file.write(f"{input('Beste gebruiker, een paar lijnen tekst in te geven: ')}\n")
+    file.seek(0)
+    f = file.readlines()
+    for num, line in enumerate(f, 1):
+        print(f'{num} - {is_containing_all_letters(line)}')
