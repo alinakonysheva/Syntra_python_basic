9bf2a6d62f7554549fcd5446c8be6950faeaa3ed
Alina Konysheva <konysheva.alina@gmail.com>
2021-12-25 14:28:18 +0100

    pension with objects

---
+++ les_9_classes_objects/inputs.py
@@ -0,0 +1,83 @@
+import re
+
+pattern_iso_str = '^[0-9]{4}-([0-9]{2}|[0-9]{1})-([0-9]{2}|[0-9]{1}&)'
+
+
+class GetInput:
+
+    @staticmethod
+    def _get_input_item(text: str, result_type: int = 0, retry_count: int = 0) -> any:
+        """get the input
+
+        Args:
+            text (str) : text to display
+            result_type (int, optional): used for converting the result to a type
+                                         0 -> default -> string
+                                         1 -> converts result to an int
+                                         2 -> convert result to float
+                                         3 -> check of the format looks like isostring, returns str if looks like that
+                                         returns None if str doesn't look like isostring
+            retry_count: asks to input 5 times and to
+
+        Returns:
+            any (int, str): result of the input
+        """
+        result = ''
+
+        try:
+            result = input(text).strip()
+            if result_type == 1:
+                result = int(result)
+            elif result_type == 2:
+                result = float(result.replace(',', '.'))
+            elif result_type == 3:
+                temp = re.match(pattern_iso_str, result)
+                if temp:
+                    result = temp.string
+                else:
+                    if retry_count < 5:
+                        print(f'you have {5 - retry_count} more attempts to give a correct input')
+                        result = GetInput._get_input_item(text, result_type, retry_count + 1)
+                    else:
+                        return None
+        except Exception as e:
+            if retry_count < 5:
+                print(f'you have {5 - retry_count} more attempts to give a correct input')
+                result = GetInput._get_input_item(text, result_type, retry_count + 1)
+            print(e)
+
+        return result
+
+    @staticmethod
+    def get_text(text: str):
+        some_text = GetInput._get_input_item(text, result_type=0)
+        return some_text
+
+    @staticmethod
+    def get_float(text: str):
+        some_float = GetInput._get_input_item(text, result_type=2)
+        return some_float
+
+    @staticmethod
+    def get_int(text: str):
+        some_int = GetInput._get_input_item(text, result_type=1)
+        return some_int
+
+    @staticmethod
+    def get_isostr(text: str):
+        some_isostr = GetInput._get_input_item(text, result_type=3)
+        return some_isostr
+
+
+def validate_number_within_range(func, arg, min_, max_, text_error):
+    try:
+        int_value = func(arg)
+        if min_ <= int_value <= max_:
+            return int_value
+        else:
+            print(text_error, f'value({int_value}) has to be within a range ({min_}, {max_})')
+            raise ValueError
+    except Exception as e:
+        print(e, f'not possible to get int from your value from {func}')
+
+
--- les_9_classes_objects/pension_with_objs.py
+++ les_9_classes_objects/pension_with_objs.py
@@ -6,6 +6,7 @@
 
 from datetime import date, timedelta, datetime
 from dateutil.relativedelta import relativedelta
+from inputs import GetInput
 
 C_TEXT_WARN = 'Probeer alstublieft nog één keer'
 
@@ -13,14 +14,14 @@ C_TEXT_WARN = 'Probeer alstublieft nog één keer'
 class Person:
     __first_name = ''
     __last_name = ''
-    __birthday = None
+    # __birthdate: date = None
     __birthplace = ''
 
-    def __init__(self, first_name: str, last_name: str, birthplace: str, birthday: str):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.birthplace = birthplace
-        self.__birthday = birthday
+    def __init__(self, first_name: str, last_name: str, birthplace: str, birthdate: str):
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__birthplace = birthplace
+        self.birthdate = birthdate
 
     @property
     def first_name(self):
@@ -48,7 +49,7 @@ class Person:
 
     @property
     def birthdate(self) -> any:
-        return self.__birthday
+        return self.__birthdate
 
     @birthdate.setter
     def birthdate(self, value: str) -> any:
@@ -63,80 +64,49 @@ class Person:
         today = date.today()
 
         try:
-            birthday = date.fromisoformat(value)
-            if birthday > today:
+            birthday_ = date.fromisoformat(value)
+            if birthday_ > today:
                 raise ValueError('Birthday can not be set later than today')
-            self.__birthday = birthday
+            self.__birthdate = birthday_
 
         except Exception as e:
             print(f'Incorrect input date string ({value}):', e)
-            self.__birthday = None
+            self.__birthdate = None
 
+    def age(self):
+        if self.__birthdate:
+            age_ = relativedelta(date.today(), self.__birthdate).years
+            return age_
+        else:
+            return None
+
+    def pension(self):
+        if self.__birthdate:
+            date_of_pension = self.__birthdate + relativedelta(months=805)
+            return date_of_pension
+        else:
+            return None
+
+    def age_18(self):
+        if self.__birthdate:
+            age_of_18 = self.__birthdate + relativedelta(years=18)
+            return age_of_18
+        else:
+            return None
+
+    def __str__(self):
+        return f'{self.__first_name} {self.__last_name}, place: {self.__birthplace}, birthday: {self.__birthdate}, ' \
+               f'age: {self.age()}, the date of your pension: {self.pension()}, you are 18 on {self.age_18()}'
 
 
 def create_person() -> Person:
-    first_name = input('your name')
-    last_name = input('your name')
-    birth_place = input('your name')
-    birth_year = input('your name')
-    birth_month = input('your name')
-    birth_day = input('your name')
-    return
+    first_name = GetInput.get_text('your first name: ')
+    last_name = GetInput.get_text('your last name: ')
+    birth_place = GetInput.get_text('your birth place: ')
+    birth_date = GetInput.get_text('your birthday in YYYY-MM-DD ')
+    p = Person(first_name, last_name, birth_place, birth_date)
+    return p
 
 
-'''for i in range(1, params_length):
-    try:
-        if i == 1:
-            year_of_user_birth = int(argv[i])
-        if i == 2:
-            month_of_user_birth = int(argv[i])
-        if i == 3:
-            day_of_user_birth = int(argv[i])
-
-    except ValueError:
-        if i == 1:
-            year_of_user_birth = int(input(f'het jaar moet realistisch zijn, gewoon 4 cijfers, {C_TEXT_WARN} {argv[i]}'))
-        elif i == 2:
-            month_of_user_birth = int(input(f'de maand was niet geldig, {C_TEXT_WARN} {argv[i]}'))
-        elif i == 3:
-            day_of_user_birth = int(input(f'de dag was niet geldig, {C_TEXT_WARN} {argv[i]}'))
-
-try:
-    birth_day = date(year_of_user_birth, month_of_user_birth, day_of_user_birth)
-except ValueError:
-    year_of_user_birth = int(input(f'het jaar moet realistisch zijn, gewoon 4 cijfers. {C_TEXT_WARN} '))
-    month_of_user_birth = int(input(f'de maand was niet geldig. {C_TEXT_WARN} '))
-    day_of_user_birth = int(input(f'de dag was niet geldig. {C_TEXT_WARN} '))
-    birth_day = date(year_of_user_birth, month_of_user_birth, day_of_user_birth)
-
-# 18 years
-year_of_18 = birth_day.year + 18
-date_18 = date(year_of_18, month_of_user_birth, day_of_user_birth)
-# pension
-date_of_pension = birth_day + relativedelta(months=805)
-
-if date_of_pension.day != birth_day.day:
-    date_of_pension += timedelta(days=1)
-# user age
-today = datetime.now()
-
-user_age = relativedelta(today, birth_day).years
-
-
-weekdays_in_dutch = {0: 'mandaag', 1: 'dinsdag', 2: 'woensdag', 3: 'donderdag',
-                     4: 'vrijdag', 5: 'zaterdag', 6: 'zondag'}
-months_in_dutch = {'January': 'januari', 'February': 'februari', 'March': 'maart', 'April': 'april', 'May': 'mei',
-                   'June': 'juni', 'July': 'juli', 'August': 'augustus', 'September': 'september',
-                   'October': 'oktober', 'November': 'november', 'December': 'december'}
-
-print(f'uw geboortedatum is {birth_day.strftime("%d")} {months_in_dutch[birth_day.strftime("%B")]} \
-{birth_day.strftime("%Y")} en dat is een {weekdays_in_dutch[birth_day.weekday()]}')
-
-print(f'u bent nu {user_age} jaar oud.')
-
-print(f'u bent 18 jaar op {date_18.strftime("%d")} {months_in_dutch[date_18.strftime("%B")]} \
-{date_18.strftime("%Y")}, en dat is een {weekdays_in_dutch[date_18.weekday()]}')
-
-print(f'u mag op pensioen gaan op {date_of_pension.strftime("%d")} {months_in_dutch[date_of_pension.strftime("%B")]} \
-{date_of_pension.strftime("%Y")}')
-'''
+pers = create_person()
+print(pers)
